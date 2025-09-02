787fba5e874752a041d7d5004ddc19f3352ca5f7
Sirisha <asirisha9457@gmail.com>
2025-09-02 11:04:23 -0500

    made changes in foodservice

--- src/main/java/com/yugi/jenkins_demo/services/foodService.java
+++ src/main/java/com/yugi/jenkins_demo/services/foodService.java
@@ -24,6 +24,7 @@ public class foodService {
     }
 
     public food getFoodById(String id) {
+        System.out.println(id);
         return foodRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException(FOOD_NOT_FOUND_STRING + id));
     }
